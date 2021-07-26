39e9e858b59c56735e6c841a3c0fdce366c71fb7
minhdc <minhdc@fabbi.io>
2021-07-26 22:27:16 +0700

    updated

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	bc := core.NewBlockChain()
-	bc.AddBlock("Send 50 MCoin to me")
-	bc.AddBlock("Send 100 Mcoin to you")
+	bc.AddBlock("You got 50 MCoin")
+	bc.AddBlock("You 25 Mcoin")
 	for _, block := range bc.Blocks {
 		fmt.Printf("Prev has: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
