53f8c572a9176b034bc1093ff9c7d9341319ff71
minhdc <minhdc@fabbi.io>
2021-07-26 22:26:25 +0700

    release ProofOfWork

--- core/block.go
+++ core/block.go
@@ -12,6 +12,7 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 
 func (b *Block) CalculateHash() {
@@ -22,7 +23,11 @@ func (b *Block) CalculateHash() {
 }
 
 func MakeBlock(data string, PrevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
-	block.CalculateHash()
+	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}, 0}
+	pow := NewPow(block)
+	nonce, hash := pow.Compute()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
--- core/pow.go
+++ core/pow.go
@@ -1,1 +1,70 @@
 package core
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+const targetBits = 20
+
+type Pow struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewPow(b *Block) *Pow {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &Pow{b, target}
+
+	return pow
+}
+
+func (pow *Pow) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func (pow *Pow) Compute() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	var maxNonce = math.MaxInt64
+	nonce := 0
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+func (pow *Pow) Validate() bool {
+	var hashInt big.Int
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
