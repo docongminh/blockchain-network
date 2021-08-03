ead40780d44344a93a4f48015b944c2b9f497240
minhdc <minhdc@fabbi.io>
2021-08-03 12:21:35 +0700

    add cli bd

--- core/block.go
+++ core/block.go
@@ -3,6 +3,8 @@ package core
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 	"strconv"
 	"time"
 )
@@ -22,7 +24,8 @@ func (b *Block) CalculateHash() {
 	b.Hash = hash[:]
 }
 
-func MakeBlock(data string, PrevBlockHash []byte) *Block {
+func NewBlock(data string, PrevBlockHash []byte) *Block {
+	// assign reference
 	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}, 0}
 	pow := NewPow(block)
 	nonce, hash := pow.Compute()
@@ -30,4 +33,25 @@ func MakeBlock(data string, PrevBlockHash []byte) *Block {
 	block.Nonce = nonce
 
 	return block
+}
+
+func (b *Block) Serializer() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result.Bytes()
+}
+
+func Deserializer(d []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
