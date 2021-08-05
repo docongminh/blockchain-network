15fc6d9a19a61585f5215c61e72284824d6cfad6
minhdc <minhdc@fabbi.io>
2021-08-06 00:22:22 +0700

    iterator interact with DB

---
+++ core/iterator.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
+// Next return next block starting from the tip
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = Deserializer(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+	i.currentHash = block.PrevBlockHash
+
+	return block
+}
