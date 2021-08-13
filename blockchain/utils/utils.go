2735e077460d7ab1215d98c36c77ea832c9edcf2
minhdc <minhdc@fabbi.io>
2021-08-13 23:52:23 +0700

    refactor

--- blockchain/utils/utils.go
+++ blockchain/utils/utils.go
@@ -26,10 +26,3 @@ func DBExist() bool {
 	}
 	return true
 }
-
-// reverse a byte array
-func ReverseBytes(data []byte) {
-	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
-		data[i], data[j] = data[j], data[i]
-	}
-}
